f43d3a2aab52303bb2a7b43dbc17695586652603
Mike.Taylor <michael.d.t.taylor@gmail.com>
2025-01-07 16:47:23 +0800

    intiial commit

---
+++ R/annotate_custom_facet.R
@@ -0,0 +1,8 @@
+annotate_custom_facet <- function (grob, xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf, data)
+{
+  layer(data = data, stat = StatIdentity, position = PositionIdentity,
+        geom = ggplot2:::GeomCustomAnn,
+        inherit.aes = TRUE, params = list(grob = grob,
+                                          xmin = xmin, xmax = xmax,
+                                          ymin = ymin, ymax = ymax))
+}
